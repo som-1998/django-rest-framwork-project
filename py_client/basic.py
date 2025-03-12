1abbc6249e186b6ce2721030f8de0e9d7afb1743
Somnath Chakraborty <115790983+som-1998@users.noreply.github.com>
2025-03-12 09:20:45 +0000

    git-ignore

--- py_client/basic.py
+++ py_client/basic.py
@@ -2,7 +2,8 @@
 
 import requests
 
-endpoint = "https://httpbin.org/anything"
+# endpoint = "https://httpbin.org/anything"
+endpoint = "http://127.0.0.1:8000/"
 
 get_responce = requests.get(endpoint, json={"key":"value"} , data= {"name":"somnath"} )
 
@@ -59,24 +60,24 @@ print(get_responce.status_code)
 # }
 
 
-{
-  "args": {}, 
-  "data": "", 
-  "files": {}, 
-  "form": {
-    "name": "somnath"
-  }, 
-  "headers": {
-    "Accept": "*/*", 
-    "Accept-Encoding": "gzip, deflate", 
-    "Content-Length": "12", 
-    "Content-Type": "application/x-www-form-urlencoded", 
-    "Host": "httpbin.org", 
-    "User-Agent": "python-requests/2.31.0", 
-    "X-Amzn-Trace-Id": "Root=1-67d04107-35f7ecd77a67f0625bce7d69"
-  }, 
-  "json": null, 
-  "method": "GET", 
-  "origin": "49.37.39.40", 
-  "url": "https://httpbin.org/anything"
-}
+# {
+#   "args": {}, 
+#   "data": "", 
+#   "files": {}, 
+#   "form": {
+#     "name": "somnath"
+#   }, 
+#   "headers": {
+#     "Accept": "*/*", 
+#     "Accept-Encoding": "gzip, deflate", 
+#     "Content-Length": "12", 
+#     "Content-Type": "application/x-www-form-urlencoded", 
+#     "Host": "httpbin.org", 
+#     "User-Agent": "python-requests/2.31.0", 
+#     "X-Amzn-Trace-Id": "Root=1-67d04107-35f7ecd77a67f0625bce7d69"
+#   }, 
+#   "json": null, 
+#   "method": "GET", 
+#   "origin": "49.37.39.40", 
+#   "url": "https://httpbin.org/anything"
+# }
