561583c5f0d3863bd8174abf88356575adc90b74
Somnath Chakraborty <115790983+som-1998@users.noreply.github.com>
2025-03-12 10:08:32 +0000

    basic

--- py_client/basic.py
+++ py_client/basic.py
@@ -4,5 +4,4 @@ import requests
 endpoint = "http://127.0.0.1:8000/api/"
 get_response = requests.get(endpoint, params={"key": "value"})
 
-print(get_response.text)
-print(get_response.status_code)
+print(get_response.json())
