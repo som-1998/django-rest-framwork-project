c7af2e07c2eed7396d66877616bf14f5b26e0b1f
Somnath Chakraborty <115790983+som-1998@users.noreply.github.com>
2025-03-12 09:58:44 +0000

    basic

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-
+import json
 
 def api_home(request, *args, **kwargs):
-    data = {
-        "name":"John Doe",
+    data1 = {
+        "name": "John Doe",
         "age": 23,
         "is_student": True
     }
+
+    try:
+        data = json.loads(request.body.decode('utf-8'))
+    except json.JSONDecodeError:
+        data = {}
+
+    print('>>>>>>', data)
+    data = data1
     return JsonResponse(data)
